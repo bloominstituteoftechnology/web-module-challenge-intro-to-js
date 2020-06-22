7ddad39f637e8db4a58a9549a5568406a39bb74a
BrityHemming <hello@brittanyhemming.com>
2020-06-22 12:08:34 -0400

    added html, js and challenges

---
+++ index.js
@@ -0,0 +1,57 @@
+/* Task 1: Warm-up!
+Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
+Task b: declare a variable and then use a conditional to change the value of that variable (no function required)
+Task c: Convert string ("1999") to integer (1999)  (no function required)
+Task d: Write a function to multiply a*b */
+
+
+/*Task 2
+Age in Dog years
+write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years*/ 
+
+/*Task 3
+Dog feeder 
+takes weight and age and returns the number of cups to feed in a day. NOTE - you may need to look up the recommended feeding requirements*/
+  
+  
+/*Task 4
+Rock, Paper, Sissors */
+// Your function should take a string (either rock paper or sissors)
+// it should return you won or you lost based on the rules of the game (you may need to look up the rules if you have not played before)
+// use math.random to determine the computers choice 
+// hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
+
+  
+  
+/*Task 5
+Metric Converter */
+//a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
+  
+//b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
+  
+  
+/* Task 6
+99 bottles of soda on the wall */
+// create a function called annoyingSong
+//the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
+  
+
+  
+/*Task 7 
+Grade Calculator */
+//write a javaScript program that takes a mark out of 100 and returns a corisponding letter grade 
+//90s should be A 
+//80s should be B 
+//70s should be Cs 
+//60s should be D 
+//and anything below 60 should be F
+  
+
+  
+  
+  /*Stretch
+  Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.*/
+  // Hint - you may need to study tomorrow's traning kit on arrays 
+  // try looking up the .includes() method
+
+  
