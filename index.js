b551f6dfb289bab186894c3987a12114aa56c3f9
Brit Hemming <hello@brittanyhemming.com>
2020-06-22 12:12:17 -0400

    Update index.js
    added additional stretch task

--- index.js
+++ index.js
@@ -54,4 +54,7 @@ Grade Calculator */
   // Hint - you may need to study tomorrow's traning kit on arrays 
   // try looking up the .includes() method
 
+/* Stretch
+Take Rock, Paper, Sissors further*/ 
+// update your rock papers sissors code below to take a prompt from a user using the window object
   
