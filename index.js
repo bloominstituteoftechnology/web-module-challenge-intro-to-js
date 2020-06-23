76e11c2cb1069863251705e5fa7264900082a25e
Brit Hemming <hello@brittanyhemming.com>
2020-06-23 12:18:28 -0400

    Update index.js
    updated task 3 with more direction - included recommended feeding

--- index.js
+++ index.js
@@ -5,15 +5,39 @@ Task c: Convert string ("1999") to integer (1999)  (no function required)
 Task d: Write a function to multiply a*b */
 
 
+
+
+
 /*Task 2
 Age in Dog years
 write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years*/ 
 
+
+
+
+
 /*Task 3
 Dog feeder 
-takes weight and age and returns the number of cups to feed in a day. NOTE - you may need to look up the recommended feeding requirements*/
+takes weight in pounds and age in years (note if the dog is a puppy the age will be a decimal) and returns the number of pounds of raw food to feed in a day. */
+
+//feeding requirements
+// adult dogs at least 1 year 
+// up to 5 lbs - 5% of their body weight
+// 6 - 10 lbs - 4% of their body weight 
+// 11 - 15 lbs - 3% of their body weight 
+// > 15lbs - 2% of their body weight 
+
+// Puppies less than 1 year
+// 2 - 4 months 10% of their body weight
+// 4 - 7 months 5% of their body weight 
+// 7 - 12 months 4% of their body weight
+
+// when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
   
-  
+
+
+
+
 /*Task 4
 Rock, Paper, Sissors */
 // Your function should take a string (either rock paper or sissors)
@@ -23,20 +47,30 @@ Rock, Paper, Sissors */
 
   
   
+
 /*Task 5
 Metric Converter */
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
-  
+
+
+
+
+
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
   
-  
+
+
+
+
 /* Task 6
 99 bottles of soda on the wall */
 // create a function called annoyingSong
 //the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
   
 
-  
+
+
+
 /*Task 7 
 Grade Calculator */
 //write a javaScript program that takes a mark out of 100 and returns a corisponding letter grade 
@@ -49,12 +83,21 @@ Grade Calculator */
 
   
   
-  /*Stretch
-  Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.*/
-  // Hint - you may need to study tomorrow's traning kit on arrays 
-  // try looking up the .includes() method
+
+/*Stretch
+Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.*/
+// Hint - you may need to study tomorrow's traning kit on arrays 
+// try looking up the .includes() method
+
+
+
+
 
 /* Stretch
 Take Rock, Paper, Sissors further*/ 
 // update your rock papers sissors code below to take a prompt from a user using the window object
-  
+
+
+
+
+
