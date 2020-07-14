069ff29b2952642570dff010535b5311a6ce1931
courtland <67969336+landcourt@users.noreply.github.com>
2020-07-14 14:50:32 -0700

    The part where I finished the warm up

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
-
-
-
-
+let votingAge = 19; {
+    if(votingAge > 18){
+        console.log(true)
+    }else{
+        console.log(false)
+    } 
+}
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
-
+let name = 'Courtland';
+    name = 'Golden'
+    console.log(name)
 
 
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
-
-
+let x = '1999';
+Number('1999')
+console.log(x)
 
 
 //Task d: Write a function to multiply a*b 
 
+function multiply(a, b){
+    return a*b;
+}
+
+console.log(multiply(1, 2));
 
 
 
