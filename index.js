fc0cfd47d61ac3fe531b0d4824678f6528a5e1a9
Francis Bonacic <francisbonacic@icloud.com>
2020-07-15 00:47:32 -0700

    was able to tackle all task but started getting typeError: console.log is not a function // had some difficulties today...

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ if (dogName = 'Terrabyte') {
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
-console.log ("task c",parseInt("1999"));  
+console.log (parseInt("1999"));  
 
 
 //Task d: Write a function to multiply a*b 
@@ -54,36 +54,25 @@ dogYears (10)
 // 11 - 15 lbs - 3% of their body weight 
 // > 15lbs - 2% of their body weight 
 
-function AdultdogFeeder (weight){
-    if (weight <= 5) {
+function dogFeeder (age, weight){
+    if (age >=1 && weight <= 5) {
         return weight * .05 
-    } else if (weight >= 6 && weight <= 10){
+    } else if (age >=1 && weight >= 6 && weight <= 10){
         return weight * .04
-    } else if (weight >= 11 && weight <= 15){
+    } else if (age >=1 && weight >= 11 && weight <= 15){
         return weight * .03;
-    } else if (weight > 15){
+    } else if (age >=1 && weight > 15){
         return weight * .02;
-    }
 }
-    console.log(AdultdogFeeder (5))
+}
+    console.log(dogFeeder (1,15))
 
 // Puppies less than 1 year
 // 2 - 4 months 10% of their body weight
 // 4 - 7 months 5% of their body weight 
 // 7 - 12 months 4% of their body weight
 
-function PuppyFeeder (months){
-    if (months >= 2 && months <= 4 ) {
-        return weight * .05 
-    } else if (weight >= 6 || weight <= 10){
-        return weight * .04
-    } else if (weight >= 11 || weight <= 15){
-        return weight * .03;
-    } else if (weight > 15){
-        return weight * .02;
-    }
-}
-    console.log(AdultdogFeeder (5))
+
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
   
@@ -99,20 +88,38 @@ function PuppyFeeder (months){
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
 
-  
-  
+
+function rockPaperScissors(){
+    let computerChoice = Math.floor(Math.random()*3)
+    if (computerChoice === 0){
+        console.log = 'rock';
+    } else if (computerChoice === 1){
+        console.log = 'paper';
+    } else if (computerChoice === 2){
+        console.log = 'scissors';
+    }    
+}
+
+rockPaperScissors ()
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
 
+//function mileConverter (km) {
+//    console.log (km * .62137119223);
+//}
 
-
+//mileConverter (5)
 
 
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
   
+//function cmConverter (feet) {
+//    console.log (feet / 0.033);
+//}
 
+//cmConverter (2)
 
 
 
@@ -121,6 +128,15 @@ function PuppyFeeder (months){
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
   
+function annoyingSong () {
+   for (let i = 99; i > 1; i--) {
+       console.log (i + 'bottles of soda on the wall, take one down pass it around', i + 'number of sodas')
+   }
+}
+
+annoyingSong ()
+
+
 
 
 
@@ -133,7 +149,13 @@ function PuppyFeeder (months){
 //70s should be Cs 
 //60s should be D 
 //and anything below 60 should be F
+
+function gradeScale (gradeValue) {
+    if (gradeValue <= 100 && gradeValue > 90);
+    console.log ('you get an A')
+}
   
+gradeScale(95)
 
   
   
