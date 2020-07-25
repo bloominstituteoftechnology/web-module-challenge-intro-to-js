59159908811ae4ae33b1572921f0d68d254cf69a
Andy Sakai <andy.sakai@outlook.com>
2020-07-24 20:28:48 -0700

    kinda got it.  Need halp

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
+var votingAge = 21;
 
+if (votingAge > 18) {
+    console.log(true)
+}
+
+
+     
 
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
 
-
-
+var age = 15
+{
+    if (age =15)
+    {
+        age = age +1;
+    }
+}
+ console.log(age)
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
 
 
+console.log(Number("1999"))
 
 
 //Task d: Write a function to multiply a*b 
 
+var a = 5;
 
+var b = 4;
+
+c = a*b;
+
+console.log(c)
 
 
 
 /************************************************************** Task 2 **************************************************************/
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
+
+var dogAge;
+
+dogAge = age*7;
 
 
 
