44883ffc2f3b131126d5f88fb2516d3dfa6e73fe
cameronyoung94 <youngcameron0794@gmail.com>
2020-07-28 14:23:40 -0400

    made some changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 let votingAge = 19;
 if (votingAge > 18) {
-    console.log("you are old enough to vote!")
+    console.log("true")
 }
 
 
@@ -34,9 +34,7 @@ console.log("1999");
 function MULTIPLY(a, b) {
     return a * b;
 }
-
-let product = MULTIPLY(5, 3);
-console.log(product);
+let product = MULTIPLY();
 
 
 
@@ -44,12 +42,11 @@ console.log(product);
 /************************************************************** Task 2 **************************************************************/
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
-function DogYears(a, b){
-    return a * b;
+function DogYears(a){
+    return a * 7;
 }
+let myAge = DogYears();
 
-let myAge = DogYears(25, 7);
-console.log(myAge);
 
 
 /************************************************************** Task 3 **************************************************************/
@@ -69,7 +66,35 @@ console.log(myAge);
 // 7 - 12 months 4% of their body weight
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
-  
+dogFeeder(15, 1)
+
+function dogFeeder(weight, age){
+
+  if (age >= 1 && weight <= 5){
+    return weight * .05 + " " + "lbs"
+  }
+  if (age >= 1 && weight >= 6 && weight <=10){
+    return weight * .04 + " " + "lbs" 
+  }
+  if (age >= 1 && weight >= 11 && weight <= 15){
+    return weight * .03 + " " + "lbs"
+  }
+  if (age >= 1 && weight >= 15){
+    return weight * .02 + " " + "lbs"
+  }
+  if (age >= 2/12 && age <= 4/12){
+    return weight * .10 + " " + "lbs"
+  }
+  if (age >= 4/12 && age <= 7/12){
+    return weight * .05 + " " + "lbs"
+  }
+  if (age >= 7/12 && age <= 1){
+    return weight * .04 + " " + "lbs"
+  }
+  console.log("Age: " + age)
+  console.log("Weight: " + weight)
+
+}
 
 
 
@@ -80,7 +105,7 @@ console.log(myAge);
 // it should return you won or you lost based on the rules of the game (you may need to look up the rules if you have not played before)
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
-function rps(input){
+function rps(){
 let userChoice = prompt ("Do you choose rock, paper or scissors?");
 console.log(userChoice);
 
@@ -94,6 +119,7 @@ if (botChoice <= 0.33) {
 } else {
     console.log("scissors");
 }
+
   if (userChoice === botChoice){
     console.log("you tied")
   } 
@@ -122,17 +148,19 @@ if (botChoice <= 0.33) {
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
+function kmConverter(){
 let kilometers = parseInt(prompt("Please enter kilometers:"));
 let miles = kilometers / 1.6;
 console.log(miles + " Miles");
-
+}
 
 
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
+function ftConverter(){
 let feet = parseInt(prompt("Please enter feet:"));
 let centimeters = feet * 30.48;
 console.log(centimeters + " centimeters");
-
+}
 
 
 
@@ -140,7 +168,7 @@ console.log(centimeters + " centimeters");
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
-function annoyingSong(input){
+function annoyingSong(){
 
 let bottles;
  for (counter = 99; counter >= 1; counter = counter - 1) 
