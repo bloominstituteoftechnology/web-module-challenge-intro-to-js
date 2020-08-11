169486dc6bd39fc5cacf066d4d04003a49dbaf03
QDG <nasha@QDGs-MacBook-Air.local>
2020-08-11 12:56:25 -0700

    added multiply function

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
-const votingAge = 18;
+const votingAge = 20;
 
-if (votingAge === 18) {
+if (votingAge > 18) {
     console.log('true')
 }
 
 
-
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
-
-
 
 
 
@@ -23,7 +20,10 @@ if (votingAge === 18) {
 
 //Task d: Write a function to multiply a*b 
 
-
+function multiply(num1, num2) {
+    return num1 * num2
+}
+    console.log(multiply(5,5));
 
 
 
