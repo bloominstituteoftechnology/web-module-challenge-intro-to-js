55e696819d1299bbb687604a7a0d85bc0627e6b0
Prince Iwejuo <princeiwejuo@gmail.com>
2020-08-13 18:02:15 -0400

    Javascriptproject

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
-
+const votingAge = 19;
+if(votingAge > 18){
+    console.log ('able to vote')
+}
 
 
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
-
-
+let car = 'on';
+let transmission = 'notWorking';
+if (transmission === 'working'){
+    car = 'on'
+}else if( transmission === 'notWorking'){
+    car = 'off';
+}
+console.log(car)
 
 
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
+console.log(Number("1999"))
+
+
+
 
 
 
 
 
 //Task d: Write a function to multiply a*b 
-
+function add(a,b){
+    return a * b;
+}
+console.log(add(6,4));
 
 
 
@@ -26,6 +42,10 @@
 /************************************************************** Task 2 **************************************************************/
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
+function add(me,dog){
+    return me * dog;
+}
+console.log(add(22,7))
 
 
 
@@ -48,9 +68,24 @@
 // 7 - 12 months 4% of their body weight
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
-  
+  function dogFeeder(weight, age){
+      if (weight <= 5 && age >=1){
+          return .005 * weight
+      } 
+       else if(weight >=6 && weight <=10 && age >=1){
+           return .004 * weight
+        }
+       else if(weight >=11 && weight <=15 && age >=1){
+           return .003 * weight
+       }
+       else if(weight >=15 && age >=1){
+           return .002 * weight
+       }
+       
+       
+  }
 
-
+console.log(dogFeeder( 15,1) )
 
 
 /************************************************************** Task 4 **************************************************************/
@@ -58,7 +93,22 @@
 // Your function should take a string (either rock paper or sissors)
 // it should return you won or you lost based on the rules of the game (you may need to look up the rules if you have not played before)
 // use math.random to determine the computers choice 
-// hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
+// hint while you can complete this with only conditionals based on strings it may help to equate choice to a number
+
+function game() {
+    const number = Math.random()
+    if (game <= .33) {
+        console.log('paper')
+    }
+    else if (game >= .34 && game <= .69) {
+        console.log('rock')
+    }
+    else if (game >= .70) {
+        console.log('scissors')
+    }
+}
+console.log(game())
+
 
   
   
@@ -67,12 +117,18 @@
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
 
-
+function calc(km,mi){
+    return(mi=km*0.62137)
+}
+    console.log(calc(4))
 
 
 
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
-  
+function calc2(ft,cm){
+    return(ft=cm*0.032808)
+} 
+    console.log(calc2(4))
 
 
 
@@ -81,7 +137,10 @@
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
-  
+  function annoyingSong(){
+      for(let i = 99; i > 0; i--)
+      console.log(i)
+  }
 
 
 
@@ -94,6 +153,27 @@
 //70s should be Cs 
 //60s should be D 
 //and anything below 60 should be F
+function gradeCalculator(marks){
+    if( marks >= 90){
+        return "A"
+    }
+    else if( marks >= 80){
+        return "B"
+    }
+    else if ( marks >= 70){
+        return "C"
+    }
+    else if ( marks >= 60){
+        return "D"
+    }
+    else{
+        return "F"
+    }
+      
+}
+console.log(gradeCalculator(78))
+
+
   
 
   
