9d5d77c7f2a40dc6ad389986c81c00ce0f2a6df4
Chelsea-Rice <cnique1995@gmail.com>
2020-09-08 17:08:41 -0500

    ANSWERED WARM UP QUESTION

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
-
-
+const votingAge = "18"
+if(votingAge >=18){
+    console.log(true);
+} else{
+    console.log(false)
+}
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
