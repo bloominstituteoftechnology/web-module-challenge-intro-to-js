3f1defa1554b878e9b95dd978a413335be764867
Sam Singer <sam.singet79@gmail.com>
2020-09-08 20:13:02 -0700

    final Java Intro Module Project

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
-//Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
+Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
 votingAge = 17 ;
 console.log(false);
 
 
 
-//Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
+Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 var taskB = 15;
 if (taskB = 15){
     taskB = 10
@@ -16,7 +16,7 @@ console.log(taskB)
 
 
 
-//Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
+Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
 var text = '1999';
 var integer = parseInt(text, 10) ;
@@ -24,7 +24,7 @@ console.log(text)
 
 
 
-//Task d: Write a function to multiply a*b 
+Task d: Write a function to multiply a*b 
 
 multiply = function(a,b) {
 Answer = a * b
@@ -36,8 +36,8 @@ console.log(Answer)
 
 
 /************************************************************** Task 2 **************************************************************/
-//Age in Dog years
-//write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
+Age in Dog years
+write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
 
 function meDog(myAge) {
 meDog = myAge * 7;
@@ -50,22 +50,22 @@ console.log(meDog)
 
 
 /************************************************************** Task 3 **************************************************************/
-//Dog feeder 
-//takes weight in pounds and age in years (note if the dog is a puppy the age will be a decimal) and returns the number of pounds of raw food to feed in a day.
+Dog feeder 
+takes weight in pounds and age in years (note if the dog is a puppy the age will be a decimal) and returns the number of pounds of raw food to feed in a day.
 
-//feeding requirements
-// adult dogs at least 1 year 
-// up to 5 lbs - 5% of their body weight
-// 6 - 10 lbs - 4% of their body weight 
-// 11 - 15 lbs - 3% of their body weight 
-// > 15lbs - 2% of their body weight 
+feeding requirements
+adult dogs at least 1 year 
+up to 5 lbs - 5% of their body weight
+6 - 10 lbs - 4% of their body weight 
+11 - 15 lbs - 3% of their body weight 
+> 15lbs - 2% of their body weight 
 
-// Puppies less than 1 year
-// 2 - 4 months 10% of their body weight
-// 4 - 7 months 5% of their body weight 
-// 7 - 12 months 4% of their body weight
+Puppies less than 1 year
+2 - 4 months 10% of their body weight
+4 - 7 months 5% of their body weight 
+7 - 12 months 4% of their body weight
 
-// when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
+when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
   
 function dogFeeder(age, weight){
     if (age >= 1 && weight >= 5){
@@ -89,21 +89,21 @@ console.log(dogFeeder(1, 9))
         
     
 
-// dogFeeder(2, 3);
-// console.log(dogFeeder);
+dogFeeder(2, 3);
+console.log(dogFeeder);
 
-// function greet(name) {
-//     console.log('Hello World ' + name);
-// }
-// greet('John');
+function greet(name) {
+    console.log('Hello World ' + name);
+}
+greet('John');
 
 
 /************************************************************** Task 4 **************************************************************/
-// Rock, Paper, Sissors
-// Your function should take a string (either rock paper or sissors)
-// it should return you won or you lost based on the rules of the game (you may need to look up the rules if you have not played before)
-// use math.random to determine the computers choice 
-// hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
+Rock, Paper, Sissors
+Your function should take a string (either rock paper or sissors)
+it should return you won or you lost based on the rules of the game (you may need to look up the rules if you have not played before)
+use math.random to determine the computers choice 
+hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
 const computerOptions = ["rock", "paper", "scissors"];
 const computerNumber = Math.floor(Math.random() * 3);
 const computerChoice = computerOptions[computerNumber];
@@ -150,55 +150,69 @@ console.log(computerChoice);
   
 
 /************************************************************** Task 5 **************************************************************/
-//Metric Converter
-//a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
+Metric Converter
+a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
+function distance(kiloms) {
+    kiloms * 0.6213712;
+   return;
+    
+}
 
 
 
 
 
-//b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
-  
+
+b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
+   function metric(feet) {
+ ////     feet * 0.032808;
+      return;
+  }
 
 
 
 
 /************************************************************** Task 6 **************************************************************/
-// 99 bottles of soda on the wall
-// create a function called annoyingSong
-// the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
-  
+99 bottles of soda on the wall
+create a function called annoyingSong
+the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
+ 
+function sodaSong() {
+    for(let i = 99; i > 1; i--) {
+        console.log("You have " + i + " bottles left");
+    }
 
+}
+console.log(sodaSong());
 
 
 
 /************************************************************** Task 7 **************************************************************/
-//Grade Calculator
-//write a javaScript program that takes a mark out of 100 and returns a corisponding letter grade 
-//90s should be A 
-//80s should be B 
-//70s should be Cs 
-//60s should be D 
-//and anything below 60 should be F
-  
+Grade Calculator
+write a javaScript program that takes a mark out of 100 and returns a corisponding letter grade 
+90s should be A 
+80s should be B 
+70s should be Cs 
+60s should be D 
+and anything below 60 should be F
 
-  
-  
+let numbGrade = 
+function gradebook (numbGrade) {
+    if (numbGrade >= 90){
+        return A
+    }   else if(numbGrade <= 90 && >= 80) {
+        return B
+    }
+        else if(numbGrade < 80 && >= 70) {
+        return C
+    }
+        else if(numbGrade < 70 && >= 60) {
+        return D
 
-/************************************************************** Stretch **************************************************************/
-//Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
-// Hint - you may need to study tomorrow's traning kit on arrays 
-// try looking up the .includes() method
-
-
-
-
-
-/************************************************************** Stretch **************************************************************/
-//Take Rock, Paper, Sissors further
-//update your rock papers sissors code below to take a prompt from a user using the window object
-
-
-
+    }
+        else () {
+        return F
+    }
 
 
+}
