e5d49dfb4f25016ed7135cd2f5facf33d52294b1
Caroline <carolinefallonlucas@gmail.com>
2020-09-11 01:45:25 -0400

    intro to javascript project submission 1

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
+const votingAge = 17;
+
+// if(votingAge > 18){
+//   console.log(true);
+// } 
+
+if (votingAge >= 18) {
+    console.log(true);
+} else {
+    console.log(false)
+}
+
 
 
 
@@ -8,18 +20,24 @@
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
 
-
+let x = 1;
+let y = 10;
+if (x + y) {
+    x++
+}
 
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
 
 
-
+Number("1999");
 
 //Task d: Write a function to multiply a*b 
 
-
+function multiply(a, b) {
+    return a * b;
+}
 
 
 
@@ -28,7 +46,9 @@
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
 
 
-
+function dogYears(yourAge) {
+    return yourAge * 7;
+}
 
 
 /************************************************************** Task 3 **************************************************************/
@@ -48,9 +68,29 @@
 // 7 - 12 months 4% of their body weight
 
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
-  
 
 
+function hungryDog(weight, age) {
+    if (age >= 1 && weight <= 5) {
+        return weight * 0.05
+    } else if (age >= 1 && weight <= 10) {
+        return weight * 0.04
+    } else if (age >= 1 && weight <= 15) {
+        return weight * 0.03
+    } else if (age >= 1 && weight < 15) {
+        return weight * 0.02
+    } else if (age < .75 && age >= 0.583) {
+        return weight * 0.05
+    } else if (age < .5 && age >= 0.333) {
+        return weight * 0.05
+    } else if (age < .33 && age >= 0.333) {
+        return weight * 0.05
+    }
+
+}
+
+
+console.log(hungryDog(15, 1))
 
 
 /************************************************************** Task 4 **************************************************************/
@@ -60,19 +100,99 @@
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
 
-  
-  
+// Get the user's choice
+const getUserChoice = (userInput) => {
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+        return userInput;
+    } else {
+        return ('Invalid User Choice');
+    }
+}
+
+
+// Get the computer's choice
+const getComputerChoice = () => {
+    randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return 'rock';
+            break;
+        case 1:
+            return 'scissors';
+            break;
+        case 2:
+            return 'paper';
+            break;
+    }
+}
+
+// Compare choices and determine winner
+const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice) {
+        return 'Tie game!';
+    }
+    if (userChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            return 'The computer won!';
+        } else {
+            return 'You won!';
+        }
+    }
+    if (userChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            return 'The computer won!';
+        } else {
+            return 'You won!';
+        }
+    }
+    if (userChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            return 'The computer won!';
+        } else {
+            return 'You won!';
+        }
+    }
+
+}
+
+// Start the game
+const playGame = () => {
+    const userChoice = getUserChoice('paper');
+    const computerChoice = getComputerChoice();
+    console.log(`You threw ${userChoice}`);
+    console.log(`The computer threw ${computerChoice}`);
+    console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
+
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
 
+// km -> mi : 0.621
+// : 1.609
+function metricConverter(km) {
+    const answer = 0.621 * km
+
+    return answer
+}
+
+// console.log(metricConverter(2))
 
 
 
 
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
-  
+function feetCM(feet) {
+    const answer = 2.54 * feet
+
+    return answer
+}
+
+console.log(feetCM(3))
 
 
 
@@ -81,7 +201,7 @@
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
-  
+
 
 
 
@@ -94,10 +214,10 @@
 //70s should be Cs 
 //60s should be D 
 //and anything below 60 should be F
-  
 
-  
-  
+
+
+
 
 /************************************************************** Stretch **************************************************************/
 //Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
