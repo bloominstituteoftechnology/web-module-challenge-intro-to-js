e8225a101c7af7b96bb2fad4cceb3e461ebc849e
Nick Watson <ngwatson79@yahoo.com>
2020-09-18 10:31:47 -0400

    Task a

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
+votingAge = Math.round(Math.random() * 80);
 
-
-
+if (votingAge > 18) {
+    console.log(true)
+} else {
+    console.log(false)
+}
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
