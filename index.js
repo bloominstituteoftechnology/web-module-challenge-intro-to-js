f9b716adc7377e47bc21b3bb66db3adcaf7408f1
Nick Watson <ngwatson79@yahoo.com>
2020-09-18 21:38:44 -0400

    Task 6

--- index.js
+++ index.js
@@ -134,9 +134,16 @@ function centimeters(num2) {
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
   
-
-
-
+function annoyingSong(num1) {
+  
+    for ( let i = num1; i > 0; i--) {
+      console.log( i + " bottles of soda on the wall, " + i + " bottles of soda, take one down, pass it around, ");
+      console.log(i - 1 +" bottles of soda on the wall!");
+  }
+  }
+  
+  
+  console.log(annoyingSong(99));
 
 /************************************************************** Task 7 **************************************************************/
 //Grade Calculator
