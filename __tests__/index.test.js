e2170475d3055c75587551094e035715592c3599
Brit Hemming <hello@brittanyhemming.com>
2020-10-06 11:46:44 -0400

    Update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -25,7 +25,7 @@ describe('hungryDogFunction', ()=>{
 });
 describe('gameFunction', ()=>{
     it('return win, lose or tie', ()=>{
-        expect(functions.game('rock', 'sissors')).toBe('you win!');
+        expect(functions.game('rock', 'scissors')).toBe('you win!');
     })
     it('return win, lose or tie', ()=>{
         expect(functions.game('rock', 'paper')).toBe('you lose!');
