a9533834968428157bf8e89f36e4e8289b5c9889
Pape Ndiaye <pape@Papes-MacBook-Pro.local>
2020-11-17 01:12:36 -0500

    MVP

--- index.js
+++ index.js
@@ -249,11 +249,21 @@ Using the grade function below do the following:
    below 60 = F
 */
   
-function grade(/*add your code here*/){
-    /*add your code here*/
+function grades(score){
+  if (score >= 90){
+    console.log("A");
+  } else if (score >= 80){
+    console.log("B");
+  } else if (score >= 70){
+    console.log("C");
+  } else if (score >= 60){
+    console.log("D");
+  } else {
+    console.log("F")
   }
-  
-  
+}
+
+grades(99);
   
   
 
