1a175a6740126e4044605cf507352addf800fff2
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2020-12-08 17:36:57 -0500

    Completed Task 2

--- index.js
+++ index.js
@@ -96,8 +96,8 @@ Do the following:
    3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-    /*add your code here*/
+function dogYears(age){ //passing an age value to the function
+    return age*7 ; //multiply age by dog years
 }
 
 
