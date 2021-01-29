556b145f4f3533dc366db9f4bd9d00bd11e2dde9
Greg Brewton <gbrewton1980@gmail.com>
2021-01-28 23:12:52 -0700

    + Task 6

--- index.js
+++ index.js
@@ -214,8 +214,11 @@ Using the annoyingSong function below do the following:
       "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong( /*add your code here*/ ) {
-    /*add your code here*/
+function annoyingSong(num) {
+    for (let i = num; i > 0; i--) {
+        console.log(`${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i--} bottles of soda on the wall`)
+            // a return cannot be used inside of a loop as a function will only return once.
+    }
 }
 
 
