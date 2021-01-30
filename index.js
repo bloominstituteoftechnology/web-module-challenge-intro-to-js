2e35bc8cb36ef05574537c47577880237f49d922
Nicole Pizzano <nicolepizzano@Nicoles-iMac.local>
2021-01-30 10:37:31 -0500

    update

--- index.js
+++ index.js
@@ -41,14 +41,16 @@ Do the following:
 */
 
 var num1 = 4;
-var num2 = 2;
+var num2 = 1;
 
-if (num1 <= num2){
-  console.log("WOAH")
+if (num1 >= num2){
+  console.log(num1 + num2)
 }
  else {
    console.log("AWWW SHOOT")
  }
+
+
 
   
 
