5c0894302e14ad7ad5b40fd621af0c3d1e8b8f20
Diego Dieterich <arancibiadiego97@gmail.com>
2021-03-01 02:19:02 -0500

    Completed Task 6

--- index.js
+++ index.js
@@ -218,10 +218,11 @@ Using the feet function below do the following:
   3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-    /*add your code here*/
+function feet(ft){
+    let cm = 30.48
+    return ft * cm
   }
- 
+ console.log(feet(2))
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -234,10 +235,10 @@ Using the annoyingSong function below do the following:
       "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-        /*add your code here*/
+function annoyingSong(number){
+        return number + " bottles of soda on the wall, " + number + " bottles of soda, take one down pass it wround " + (number - 1) + " bottles of soda on the wall";
   }
-
+console.log(annoyingSong(100));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
