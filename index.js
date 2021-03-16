66ad3ae889784f36e2c016b3761a3b7c0340e723
alvaro <alvarofloresmora17@gmail.com>
2021-03-16 15:01:18 -0500

    solved Task #4

--- index.js
+++ index.js
@@ -133,11 +133,33 @@ Use the game function below to do the following:
   
   HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
+let computer = Math.random();
+if(computer <= 0.34){
+  computer = 'rock';
+} else if(computer <= 0.67){
+  computer = 'paper';
+}else if(computer > 0.67){
+  computer = 'scissors';
+}
+console.log(computer);
 
 function game(user, computer){
-    /*add your code here*/
+    if(user === computer){
+      return `it's a tie`;
+    }else if(user === 'rock' && computer === 'scissors'){
+      return `you win!`;
+    }else if(user === 'paper' && computer === 'rock'){
+      return `you win!`;
+    }else if(user === 'scissors' && computer === 'paper'){
+      return `you win!`;
+    }else{
+      return `you lose!`;
+    }
 }
-  
+
+
+
+
   
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
