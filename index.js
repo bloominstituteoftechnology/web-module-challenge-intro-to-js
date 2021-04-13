6d59cff3b11b2fceb16c4ebbe0188b856c2fb5e8
everybodyneedsis <jacobkylefranklin@gmail.com>
2021-04-13 17:57:58 -0400

    finished up my last two tasks

--- index.js
+++ index.js
@@ -252,10 +252,8 @@ Using the annoyingSong function below do the following:
 */
 
 function annoyingSong(number){
-        for(let i = 0; i >= 99; i--){
           return (`${number} bottles of soda on the wall, ${number} bottles of soda, take one down pass it around ${number - 1} bottles of soda on the wall`)
         }
-  }
 
   console.log(annoyingSong(67));
 
@@ -275,21 +273,38 @@ Using the grade function below do the following:
    below should return 'you got an F'
 */
   
-function grade(score){
-  if(score <= 90 && score >= 100){
-    return `you got an A`;
-  } else if(score <= 80 && >= 89){
-    return `you got a B`;
-  } else if(score <= 70 && >= 79){
-    return `you got a C`;
-  } else if(score <=60 && score >= 69){
-    return `you got a D`;
-  } else {
+// function grade(score){
+//   if(score <= 100 && score >= 90){
+//     return `you got an A`;
+//   } else if(score >= 80 && score <= 89){
+//     return `you got a B`;
+//   } else if(score >= 70 && score <= 79){
+//     return `you got a C`;
+//   } else if(score >=60 && score <= 69){
+//     return `you got a D`;
+//   } else {
+//     return `you got an F`;
+//   }
+// }
+
+
+
+
+function grade(score) {
+  if(score < 60) {
     return `you got an F`;
+  } else if(score <= 69){
+    return `you got a D`;
+  } else if(score <= 79){
+    return `you got a C`;
+  } else if(score <= 89){
+    return `you got a B`;
+  } else{
+    return `you got an A`
   }
 }
 
-console.log(grade(82))
+console.log(grade(80))
 
 
 
