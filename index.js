6f00bfaccba2d20e0a2e4066c40f2459a9b8a9cc
Michael Karel <kare6283@gmail.com>
2021-06-09 00:51:09 -0600

    Completed first function

--- index.js
+++ index.js
@@ -58,9 +58,11 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(/*add your code here*/){
-    /*add your code here*/
+function multiply(a, b){
+  return a * b;
   }
+
+  console.log(multiply(1,3));
 
 
 
