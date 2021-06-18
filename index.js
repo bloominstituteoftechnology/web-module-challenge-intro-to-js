522af0036d410667ea274b10b7a80d261282b60d
unknown <aaron.fuller@protonmail.com>
2021-06-17 23:45:56 -0400

    Finished task 6

--- index.js
+++ index.js
@@ -175,9 +175,14 @@ feetToCent(5);
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
-  
 
+function bottles (startingNumber) {
+    for (i = startingNumber; i >= 1; i--){
+        console.log(`${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i - 1} bottles of soda on the wall`);
+    }
+}
 
+bottles(5);
 
 
 /************************************************************** Task 7 **************************************************************/
