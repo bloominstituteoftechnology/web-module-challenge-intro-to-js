d7160685ecfaee17b9ea3e7b3d8cc09a29c6a9e4
BrityHemming <hello@brittanyhemming.com>
2021-06-22 15:12:10 -0400

    clarified return instructions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /*🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️ Task 1: Warm-up! 🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️*/
 
-/*MAKE SURE TO RETURN ALL OF THE ANSWERS INSIDE OF A FUNCTION ON THESE TASKS, IF YOU DON'T, THE AUTOGRADER WILL NOT WORK*/
+/*MAKE SURE TO RETURN ALL OF THE ANSWERS INSIDE OF A FUNCTION (tasks 1d through task 7), IF YOU DON'T, THE AUTOGRADER WILL NOT WORK*/
+
+// task 1a, 1b, and 1c are not autograded
 
 /*
 When doing these tasks, we recommend using console.log to test the output of your code to make sure it works correctly.
