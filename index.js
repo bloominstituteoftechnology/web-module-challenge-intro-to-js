362955a45b4203b509d3d4bd49e56fccf65dd6e8
AdamI19 <broadscars@gmail.com>
2021-07-13 14:53:18 -0600

    solved tasks 3, 5, and 7

--- index.js
+++ index.js
@@ -128,9 +128,27 @@ Puppies less than 1 year
 NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
 */  
 
-function hungryDog(/*add your code here*/){
-  /*add your code here*/
+
+function hungryDog(weight, age){
+  if (age >= 1) {
+    if (weight <= 5) {
+      return weight * 0.0583;
+    } else if (weight < 10) {
+      return weight * 0.04;
+    } else if (weight <= 15) {
+      return weight * 0.03;
+    } else {
+      return weight * 0.02;
+    }
+  } else if (age >= 2 / 12 && age <= 4 / 12) {
+    return weight * 0.1;
+  } else if (age > 4 / 12 && age <= 7 / 12) {
+    return weight * 0.05;
+  } else if (age > 7 / 12 && age < 1) {
+    return weight * 0.04;
+  }
 }
+console.log(hungryDog(15,1));
 
 
 
@@ -190,10 +208,13 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
-}
 
+
+function miles(Convert, miles){
+  return (Convert*miles);
+}
+let kilometers= miles(1.60934,2);
+console.log (kilometers);
 
 
 //Task 5b - Feet to CM
@@ -204,9 +225,11 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(Convert,CM){
+  return (convert*CM);
 }
+let Centimeters= feet(30.48,10);
+console.log(Centimeters)
 
 
 
@@ -243,9 +266,21 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(number){
+if grade < 60 {
+  return "you got an F";
+}else if (grade < 70) {
+  return "you got a D"; 
+    } 
+else if (grade < 80) 
+{
+  return "you got a C"; 
+} else if (grade < 90) {
+  return "you got a B"; 
+} else if (grade < 100) {
+  return "you got an A";
 }
+console.log(grade 80);
 
 
 
