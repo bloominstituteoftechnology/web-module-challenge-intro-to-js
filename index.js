954ef02657a0abc3916bd3ff84e9e8df61e7c46e
Andrew Gary <andrew.gary91@gmail.com>
2021-11-02 14:20:07 -0500

    reset up project and working my way through the functions

--- index.js
+++ index.js
@@ -12,11 +12,9 @@ returns a value, that value will be logged to the console.  An example of this w
 
 /*
 Task 1a - Voting Age
-
 Do the following:   
    1. Create a variable called votingAge and assign it a value
    2. Console log true if age is 18 or higher
-
    HINT: no function required
 */
 const votingAge = 18;
@@ -27,31 +25,33 @@ if(votingAge >= 18){
 
 /*
 Task 1b - Values
-
 Do the following:   
    1. Declare two variables and assign them values
    2. Use a conditional to change the value of the 1st variable based on the value assigned to the 2nd variable
    3. Console log the new value of the 1st variable
-
    HINT: no function required
 */
 
+let myName = 'Andrew Gary';
+let myAge = 30;
 
+if(myAge > 2){
+  myName = 'Gary Andrew';
+}
 
 
 
 /*
 Task 1c - Convert Strings to Numbers
-
 Do the following:   
    1. Declare a string type variable with the value of "1999"
    2. Convert the string value of "1999" to a integer value of 1999
    3. Console log the result
-
    HINT: look up the Number method
 */
 
-
+let year = '1999';
+year = parseInt('year');
 
 
 /*
@@ -93,17 +93,13 @@ Use the hungryDog function and feeding requirements below to do the following:
 1. Invoke the hungryDog function below and pass it a weight value in pounds
 2. Also, pass to hungryDog an age value in years (note: if the dog is a puppy, the age will be a decimal. For example: three months = 3/12 or .25)
 3. Do the proper calculations and return the number of pounds of raw food to feed the dog/puppy in a day
-
 REMEMBER: This program should handle correctly adult AND puppy ages and weights
-
 Feeding Requirements:
-
 Adult Dogs 1 year and older 
    up to 5 lbs - 5% of their body weight
    6 - 10 lbs - 4% of their body weight 
    11 - 15 lbs - 3% of their body weight 
    > 15lbs - 2% of their body weight 
-
 Puppies less than 1 year
    2 - 4 months 10% of their body weight
    4 - 7 months 5% of their body weight 
@@ -112,25 +108,9 @@ Puppies less than 1 year
 NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
 */  
 
-function hungryDog(weight, age){
-  if(age >= 1){
-    if(weight <= 5){
-      return weight * .05;
-    } else if(weight >= 6 && weight <= 10){
-      return weight * .04;
-    } else if(weight >= 11 && weight <= 15){
-      return weight * .03;
-    } else{
-      return weight * .02;
-    }
-  }else if (age >= (2 / 12) && age <= (4 / 12)){
-    return weight * .1;
-  }else if (age > (4/12) && age <= (7 / 12)){
-    return weight * .05;
-  }else if (age > (7 / 12) && age < 1){
-    return weight * .04;
-  }
-  
+function hungryDog(/*add your code here*/){
+  /*add your code here*/
+}
 
 
 
@@ -140,23 +120,19 @@ function hungryDog(weight, age){
 /*
 Create a global variable that randomly generates the computer's choice
 Use Math.random to determine the computers choice (Math.random gives a random number between 0 and 1)
-
 HINT: While you can complete this with only conditionals based on strings, it may help to equate choice to a number when using Math.random()
-
 Use the game function below to do the following:
 1. Receive 2 parameters the user's choice and the computer's choice
 2. Return whether the user won, lost, or tied based on these rules of the game described below - the strings returned need to match the strings below exactly.
  - win should return "you win!"
  - lose should return "you lose!"
  - tie should return "it's a tie"
-
 RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
-
 HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
 
 function game(user, computer){
-  /*add your code here*/
+
 }
 
 
@@ -172,8 +148,8 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(kilometers){
+  return kilometers * .621371;
 }
 
 
@@ -186,8 +162,8 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm / 30.48; 
 }
 
 
@@ -202,8 +178,8 @@ Using the annoyingSong function below do the following:
     "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(startingNumber){
+      return startingNumber + ' bottles of soda on the wall, ' + startingNumber + ' bottles of soda, take one down pass it around ' + (startingNumber - 1) + ' bottles of soda on the wall';
 }
 
 
@@ -214,7 +190,6 @@ function annoyingSong(/*add your code here*/){
 Using the grade function below do the following: 
 1. Receive a score out of 100 
 2. Return the corresponding letter grade following this grade scale:
-
  90-100 should return 'you got an A' 
  80-89 should return 'you got a B'
  70-79 should return 'you got a C'
@@ -222,8 +197,18 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(grade){
+  if(grade > 89){
+    return 'you got an A';
+  }else if( grade > 79 && grade < 90){
+    return 'you got a B';
+  }else if( grade > 69 && grade < 80){
+    return 'you got a C';
+  }else if( grade > 59 && grade < 70){
+    return 'you got a D';
+  }else{
+    return 'you got an F';
+  }
 }
 
 
@@ -235,7 +220,6 @@ function grade(/*Your Code here */){
 Using the vowelCounter function below do the following:
 1. Receive a string as a parameter
 2. Count and return the number of vowels within that string.  It should handle both capitalized and uncapitalized vowels.
-
 HINT - you may need to study tomorrow's content on arrays 
 HINT - try looking up the .includes() method
 */
