ca866aa955025c9c2e071cb284b8459c5600f63a
Cory Riley <ceriley23@gmail.com>
2022-08-07 17:57:01 -0400

    first push, 10 passed

--- index.js
+++ index.js
@@ -19,7 +19,13 @@ Do the following:
 
    HINT: no function required
 */
+const votingAge = 17
 
+if (votingAge >= 18){
+  console.log(true)
+} else{
+  console.log(false)
+}
 
 
 /*
@@ -33,9 +39,15 @@ Do the following:
 
    HINT: no function required
 */
+let firstThing = 1
+let secondThing = 2
 
+if(secondThing === 2){
+  firstThing = 3;
+} else {firstThing = 1
+}
 
-
+console.log('task 1b:', firstThing)
 
 
 /*
@@ -48,7 +60,8 @@ Do the following:
 
    HINT: look up the Number method
 */
-
+const party = '1999';
+console.log('task 1c:', Number(party))
 
 
 
@@ -61,11 +74,11 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(/*add your code here*/){
-  /*add your code here*/
+function multiply(a, b){
+  return a * b;
 }
 
-
+console.log('task 1d:', multiply(8,8))
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -77,10 +90,10 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
+function dogYears(age){
+  return age * 7;
 }
-
+console.log('task 1d:', dogYears(10))
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -129,11 +142,27 @@ NOTE 2: This is a great time to check the tests to see what it expects, versus w
         So, on this one test, the weight would be 4 pounds, and the age would be 1 years old. It's expecting your function to return a decimal number of 0.2
 */  
 
-function hungryDog(/*add your code here*/){
-  /*add your code here*/
+function hungryDog(weight, age){
+  if(age >= 1 && weight <= 5){
+    return weight * 0.05;
+  }else if(age >= 1 && weight >= 6 && weight <= 10){
+    return weight * 0.04;
+  }else if(age >= 1 && weight >= 11 && weight <= 15){
+    return weight * 0.03;
+  }else if (age >= 1 && weight > 15){
+    return weight * 0.02;
+  }else if (age < 1 && age >= 0.583){
+    return weight * 0.04;
+  }else if (age < 0.583 && age >= 0.333){
+    return weight * 0.05;
+  }else if (age < 0.333){
+    return weight * 0.1;
+  }else{
+    return 'error, try again'
+  }
 }
 
-
+console.log('task 3', hungryDog(15, 1))
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
