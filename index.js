36384c9e2fa3a6b84332299b80c0d5010cf15bee
jaredhuffer1 <jaredhuffer1@gmail.com>
2022-10-07 14:00:40 -0400

    Done!

--- index.js
+++ index.js
@@ -276,7 +276,7 @@ Using the annoyingSong function below do the following:
 
 function annoyingSong(number){
       for(let i = number; i > 0; i --){
-        return `${i}} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i -1 } bottles of soda on the wall`;
+        return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i -1} bottles of soda on the wall`;
       }
   }
 console.log('task 6', annoyingSong(5));
